0740f800fb4917b2180bf1819731ce684c686276
mauricio <mauricio.vera.r7@gmail.com>
2021-10-08 23:11:40 -0300

    v.1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      ><!---->
+      <v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>Mi App<!--hamburguesa-->
+        <v-spacer></v-spacer
+        ><!--propiedades flex de css separa a la derecha-->
+        <v-toolbar-items class="hidden-sm-and-down"
+          ><!-- agrupa a los elementos de navegación-->
+          <v-btn text>Inicio</v-btn>
+          <v-btn text>Buscar</v-btn>
+          <v-btn text>Acerca de</v-btn>
+        </v-toolbar-items>
+      </v-app-bar>
+    </v-app-bar>
+
+    <v-container class="fill-height" fluid style="min-height: 434px">
+      <v-fade-transition mode="out-in">
+        <v-row class="row px-16">
+          <v-col cols="4" v-for = "(item, index) in personajes" :key = "index">
+            <v-card>
+              <v-img
+                :src="item.imagen"
+                height="420"
+                class="grey darken-4"
+              ></v-img>
+              <v-card-title class="text-h6"> {{item.nombre}} </v-card-title>
+            </v-card>
+          </v-col>          
+        </v-row>
+      </v-fade-transition>
+    </v-container>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      personajes: [
+      {
+      'id': 1,
+      'nombre': 'Morty',
+      'imagen':
+      'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+      },
+      {
+      'id': 2,
+      'nombre': 'Rick',
+      'imagen':
+      'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+      },
+      {
+      'id': 3,
+      'nombre': 'Summer',
+      'imagen':
+      'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+      },
+      {
+      'id': 4,
+      'nombre': 'Beth',
+      'imagen':
+      'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+      },
+      {
+      'id': 5,
+      'nombre': 'Jerry',
+      'imagen':
+      'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+      }
+      ]
+    }
+  }
+};
+</script>
